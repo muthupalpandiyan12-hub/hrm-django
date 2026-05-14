ff5600f93260e92107bd05922cd90b8076481846
muthu <muthupalpandiyan.12@gmail.com>
2026-05-14 15:05:43 +0530

    Use bare minimum admin configuration - default only

--- employees/admin.py
+++ employees/admin.py
@@ -1,21 +1,7 @@
 from django.contrib import admin
 from .models import Employee, Department, SalaryHistory
 
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('employee_id', 'name', 'email', 'position', 'status')
-    list_filter = ('status', 'gender')
-    search_fields = ('name', 'email', 'employee_id')
-
-
-@admin.register(SalaryHistory)
-class SalaryHistoryAdmin(admin.ModelAdmin):
-    list_display = ('employee', 'old_salary', 'new_salary', 'effective_date')
-    search_fields = ('employee__name',)
+# Register models with default admin - no customization
+admin.site.register(Department)
+admin.site.register(Employee)
+admin.site.register(SalaryHistory)
